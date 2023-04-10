55c4e892c8850ef9f2e769b410683e19ad6f1092
haz3141 <38766403+haz3141@users.noreply.github.com>
2023-04-10 17:34:14 -0400

    Created deposit function

--- Wallet.sol
+++ Wallet.sol
@@ -8,5 +8,21 @@ contract Wallet {
         uint accountId;
     }
 
-    Account[] public accounts;
+    mapping (address => bool) depositors;
+    mapping (address => Account) public accounts;
+    uint private totalDeposits = 0;
+    uint private totalBalance = 0;
+
+    function depositEther() public payable {
+        require(msg.value > 0, "Must deposit more than 0.");
+
+        if (depositors[msg.sender] == false) {
+            depositors[msg.sender] = true;
+            Account memory newDepositor = Account(msg.sender, msg.value, totalDeposits);
+            accounts[msg.sender] = newDepositor;
+        } else {
+            Account storage account = accounts[msg.sender];
+            account.etherBalance += msg.value;
+        }
+    }
 }
