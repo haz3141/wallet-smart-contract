17d95f5b7ffab669bd7221c5083814c79c7dd22c
haz3141 <38766403+haz3141@users.noreply.github.com>
2023-04-10 16:28:17 -0400

    Added account structure

--- Wallet.sol
+++ Wallet.sol
@@ -1,2 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
+
+contract Wallet {
+    struct Account {
+        address accountOwner;
+        uint etherBalance;
+        uint accountId;
+    }
+
+    Account[] public accounts;
+}
